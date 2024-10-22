e54c37fe966a32a1b7b523a0158e90347b6fe49f
Aravindh <122135451+Aravindh0@users.noreply.github.com>
2024-10-22 14:44:31 +0530

    Time: 1278 ms (68.58%), Space: 0B (100%) - LeetHub

---
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -0,0 +1,15 @@
+/* Write your T-SQL query statement below */
+
+    
+SELECT 
+    p.project_id,
+    ROUND(AVG(e.experience_years*1.0), 2) AS average_years
+FROM 
+    Project AS p
+LEFT JOIN 
+    Employee AS e
+ON 
+    p.employee_id = e.employee_id
+GROUP BY 
+    p.project_id;
+
