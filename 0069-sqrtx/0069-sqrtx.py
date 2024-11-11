7eeff147fcb8e58b00aef296cbc171fa227a75a7
Aravindh <122135451+Aravindh0@users.noreply.github.com>
2024-11-11 14:32:06 +0530

    Time: 0 ms (100%), Space: 16.5 MB (53.13%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,6 +1,19 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        from math import sqrt,floor
-        return floor(sqrt(x))
-
+        if x == 0 or x == 1:
+            return x
         
+        # Binary search to find the square root
+        left, right = 1, x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+                result = mid
+            else:
+                right = mid - 1
+        
+        return result
+     
